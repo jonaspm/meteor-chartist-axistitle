c9c4ac8a84bb4b88274241428e7f3df0eadd573b
Jonas Perusquia <jonaspm_99@outlook.com>
2016-05-05 17:44:41 -0600

    Add dependecy

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
-  name: 'jonaspm:chartist-axistitle',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: 'Chartist plugin AxisTitle for using with angular-chartist',
-  // URL to the Git repository containing the source code for this package.
-  git: ''
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  //documentation: 'README.md'
+	name: 'jonaspm:chartist-axistitle',
+	version: '0.0.1',
+	// Brief, one-line summary of the package.
+	summary: 'Chartist plugin AxisTitle for using with angular-chartist',
+	// URL to the Git repository containing the source code for this package.
+	git: 'https://github.com/jonaspm/meteor-chartist-axistitle.git',
+	// By default, Meteor will default to using README.md for documentation.
+	// To avoid submitting documentation, set this field to null.
+	documentation: 'README.md'
 });
 
 Npm.depends({
@@ -15,14 +15,15 @@ Npm.depends({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
-  api.use('ecmascript');
-  api.addFiles('chartist-axistitle.js', 'client');
+	api.versionsFrom('1.2.1');
+	api.use('ecmascript');
+	api.use('mfpierre:chartist-js','client')
+	api.addFiles('chartist-axistitle.js', 'client');
 });
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('chartist-axistitle');
-  api.addFiles('chartist-axistitle-tests.js');
+	api.use('ecmascript');
+	api.use('tinytest');
+	api.use('jonaspm:chartist-axistitle');
+	api.addFiles('chartist-axistitle-tests.js');
 });
